c9f287f1fd4370993b1cfde7e459dfe9c2a255e8
John Fenix <fenix@noindoin.com>
2015-04-14 15:53:15 +0800

    v0.3.0

--- yalt-util.js
+++ yalt-util.js
@@ -1,6 +1,6 @@
 if (typeof module != 'undefined' && module.exports) { // node
-	var yalt = require('yalt');
-	module.exports = yalt;
+  var yalt = require('yalt');
+  module.exports = yalt;
 }
 
 
@@ -8,30 +8,30 @@ if (typeof module != 'undefined' && module.exports) { // node
 Parse the Accept-Language HTTP header.
 
 Parameters:
-	header: String. e.g. 'en-US,zh-CN;q=0.5,zh-Hans-CN;q=0.5'
+  header: String. e.g. 'en-US,zh-CN;q=0.5,zh-Hans-CN;q=0.5'
 
 Returns: Array. Sort by quality.
-	e.g. ['en-US', 'zh-CN', 'zh-Hans-CN']
+  e.g. ['en-US', 'zh-CN', 'zh-Hans-CN']
 */
 
 yalt.parseAcceptLanguage = function(header) {
-	if (!header)
-		return [];
+  if (!header)
+    return [];
 
-	var langs = [];
-	var pattern = /([a-z]+(?:-[a-z]+)*) *(?:; *q *= *(1|0\.[0-9]+))?/ig;
-	var result;
+  var langs = [];
+  var pattern = /([a-z]+(?:-[a-z]+)*) *(?:; *q *= *(1|0\.[0-9]+))?/ig;
+  var result;
 
-	while (result = pattern.exec(header))
-		langs.push([result[1], Number(result[2] || 1)]);
-	
-	langs.sort(function(a, b) {
-		return b[1] - a[1];
-	});
+  while (result = pattern.exec(header))
+    langs.push([result[1], Number(result[2] || 1)]);
+  
+  langs.sort(function(a, b) {
+    return b[1] - a[1];
+  });
 
-	return langs.map(function(item) {
-		return item[0];
-	});
+  return langs.map(function(item) {
+    return item[0];
+  });
 };
 
 
@@ -39,38 +39,38 @@ yalt.parseAcceptLanguage = function(header) {
 Select the most preferred language available.
 
 Parameters:
-	available: Array. Available languages. If an item is array, returns the first language if anyone matches the accept languages.  e.g.
-		[
-			'ja',
-			['en-US', 'en'],
-			['zh-TW', 'zh-HK', 'zh-Hant'],
-			['zh-CN', 'zh-Hans', 'zh'] // this entry must be placed after "zh-TW", because "zh" will match all "zh-*" tags
-		]
+  available: Array. Available languages. If an item is array, returns the first language if anyone matches the accept languages.  e.g.
+    [
+      'ja',
+      ['en-US', 'en'],
+      ['zh-TW', 'zh-HK', 'zh-Hant'],
+      ['zh-CN', 'zh-Hans', 'zh'] // this entry must be placed after "zh-TW", because "zh" will match all "zh-*" tags
+    ]
 
-	accept: Array. Accept Languages. Return value of yalt.parseAcceptLanguage().
+  accept: Array. Accept Languages. Return value of yalt.parseAcceptLanguage().
 
 Returns: String. The highest quality language available. If none matches, returns the first language in avaliable languages.
 */
 
 yalt.selectLanguage = function(available, accept) {
-	for (var i = 0; i < accept.length; i++) {
-		var ac = accept[i].toLowerCase();
-		for (var j = 0; j < available.length; j++) {
-			var av = available[j];
-			if (av.constructor == String) {
-				var avlc = av.toLowerCase();
-				if (ac.indexOf(avlc) == 0 || avlc.indexOf(ac) == 0)
-					return av;
-			} else {
-				var a = av[0];
-				for (var k = 0; k < av.length; k++) {
-					var b = av[k].toLowerCase();
-					if (ac.indexOf(b) == 0 || b.indexOf(ac) == 0)
-						return a;
-				}
-			}
-		}
-	}
+  for (var i = 0; i < accept.length; i++) {
+    var ac = accept[i].toLowerCase();
+    for (var j = 0; j < available.length; j++) {
+      var av = available[j];
+      if (av.constructor == String) {
+        var avlc = av.toLowerCase();
+        if (ac.indexOf(avlc) == 0 || avlc.indexOf(ac) == 0)
+          return av;
+      } else {
+        var a = av[0];
+        for (var k = 0; k < av.length; k++) {
+          var b = av[k].toLowerCase();
+          if (ac.indexOf(b) == 0 || b.indexOf(ac) == 0)
+            return a;
+        }
+      }
+    }
+  }
 
-	return available[0].constructor == String ? available[0] : available[0][0];
+  return available[0].constructor == String ? available[0] : available[0][0];
 };
--- yalt.js
+++ yalt.js
@@ -1,34 +1,52 @@
-function yalt(code, dict) {
-  if (!yalt.dict[code]) {
-    yalt.dict[code] = function(msg) {
-      if (d[msg] != null)
-        return d[msg].constructor == String ? vsprintf(d[msg], Array.prototype.slice.call(arguments, 1)) : d[msg].apply(d, Array.prototype.slice.call(arguments, 1));
-      else if (d != yalt.fallback)
-        return yalt.fallback.apply(yalt.fallback, arguments);
-      else
-        return msg;
+var yalt = {
+  add: function(code, dict, domain, isFallback) {
+    if (!domain)
+      domain = '_global';
+
+    if (!yalt.dicts[domain])
+      yalt.dicts[domain] = {};
+
+    if (!yalt.dicts[domain][code])
+      yalt.dicts[domain][code] = {};
+
+    for (var p in dict)
+      yalt.dicts[domain][code][p] = dict[p];
+
+    if (isFallback || !yalt.dicts[domain]._fallback)
+      yalt.dicts[domain]._fallback = yalt.dicts[domain][code];
+  },
+
+  get: function(code, domain) {
+    if (!domain)
+      domain = '_global';
+
+    function gettext(msg) {
+      var params = Array.prototype.slice.call(arguments, 1);
+
+      for (var i = 0; i < gettext.dicts.length; i++) {
+        var dict = gettext.dicts[i];
+        if (dict[msg])
+          return dict[msg].constructor == String ? vsprintf(dict[msg], params) : dict[msg].apply(null, params);
+      }
+
+      return msg;
+    }
+
+    gettext.set = function(code) {
+      gettext.dicts = [yalt.dicts[domain][code]];
+      if (yalt.dicts[domain][code] != yalt.dicts[domain]._fallback)
+        gettext.dicts.push(yalt.dicts[domain]._fallback);
     };
-    if (!yalt.fallback)
-      yalt.fallback = yalt.dict[code];
-  }
-  var d = yalt.current = yalt.dict[code];
-  if (dict) {
-    for (var i in dict)
-      d[i] = dict[i];
-    dict = null;
-  }
-  return yalt.dict[code];
-}
 
-yalt.dict = {};
+    gettext.set(code);
 
-yalt.gettext = function() {
-  return yalt.current.apply(yalt.current, arguments);
+    return gettext;
+  },
+
+  dicts: {}
 };
 
-if (typeof window != 'undefined') {
-  window.__ = yalt.gettext;
-} else if (typeof module != 'undefined' && module.exports) { // node
+if (typeof module != 'undefined' && module.exports) { // node
   var vsprintf = require('sprintf-js').vsprintf;
   module.exports = yalt;
   require('yalt/yalt-util');
